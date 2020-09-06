f59215ff3aa11709d3e1658febcdfe3cd7ef4396
Victor H. Ayala Sanchez <58852073+Vic16@users.noreply.github.com>
2020-09-05 23:50:26 -0300

    Delete script.R

--- script.R
+++
@@ -1,6 +0,0 @@
-players$passportArea %>%
-  group_by(alpha3code) %>%
-  summarise(Frecuencia = n()) %>%
-  mutate(Porcentaje = round(prop.table(Frecuencia),2))%>%
-  arrange(desc(Porcentaje)) %>%
-  head(40)
